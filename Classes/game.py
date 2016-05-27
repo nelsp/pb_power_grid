5c34b5f54bcdf2d90b8517d136b235f99c625639
Erik Brakke <eriksbrakke@gmail.com>
2016-05-27 12:20:04 -0400

    Added optional parameter for number of players in the deck setup.  Defaults to 5 currently

--- Classes/game.py
+++ Classes/game.py
@@ -12,11 +12,21 @@ class Game:
 		self.resources = props.get('resources')
 
 	@classmethod
-	def create_initial_deck(cls, cards):
+	def create_initial_deck(cls, cards, num_players=5):
 		light_cards = [c for c in cards if c.type == 'light']
 		dark_cards = [c for c in cards if c.type == 'dark']
 		
 		shuffle(dark_cards)
+		shuffle(light_cards)
+		
+		#Remove cards if playing with less players
+		if num_players == 3:
+			dark_cards = dark_cards[:-2]
+			light_cards = light_cards[:-6]
+		if num_players == 4:
+			dark_cards = dark_cards[:-1]
+			light_cards = light_cards[:-3]
+		
 		first_nine = sorted(dark_cards[:9], key=lambda x: x.cost)
 		rest = dark_cards[9:] + light_cards
 		shuffle(rest)
